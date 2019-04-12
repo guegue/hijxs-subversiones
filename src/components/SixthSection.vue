0b1bc7b807fc6c0c169c61ba33bc5a134e683b7d
fernando <josefernandohernandezpaz@gmail.com>
2019-04-12 10:02:52 -0600

    .

--- src/components/SixthSection.vue
+++ src/components/SixthSection.vue
@@ -12,9 +12,11 @@
         <!--</b-row>-->
         <top-bar :flag="false" class="ml-0 mr-0 pt-5"></top-bar>
 
-        <b-row class="justify-content-center text-right pr-5 text-white">
+        <b-row class="justify-content-center text-right p-3 mr-5 text-white">
             <b-col sm="12" md="12" lg="12">
-                <small> &copy; Todos los derechos reservados, {{ new Date().getFullYear() }}</small>
+                <b-container class="p-2">
+                    <small> &copy; Todos los derechos reservados, {{ new Date().getFullYear() }}</small>
+                </b-container>
             </b-col>
         </b-row>
     </b-container>
